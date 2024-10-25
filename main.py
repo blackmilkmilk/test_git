e521e5c0a42c483786c0a2a69f227850d87e19f2
Margo Rudenko <margo@loona.com>
2024-10-25 18:14:06 +0200

    merged brunches, added random pauses

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
+import random
+
 
 def print_hi(name, age):
     for i in range(10):
         print(f'Hello, {name}')
+        time.random.randint(1, 3)
         if i//2 == 0:
             print(f'your age is {age}')
 

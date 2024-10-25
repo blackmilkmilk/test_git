a382270d686c8b55f4c5ae28294846b786ec544b
Margo Rudenko <margo@loona.com>
2024-10-25 18:10:29 +0200

    added age and even number check

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
-def print_hi(name):
+def print_hi(name, age):
     for i in range(10):
         print(f'Hello, {name}')
+        if i//2 == 0:
+            print(f'your age is {age}')
 
 
 if __name__ == '__main__':
